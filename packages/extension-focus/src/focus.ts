094511af0e368a0b18a1096dc793bf2b3c05c8cc
liudawei <liudawei@smzdm.com>
2023-06-06 23:22:46 +0800

    添加注释

--- packages/extension-focus/src/focus.ts
+++ packages/extension-focus/src/focus.ts
@@ -4,7 +4,7 @@ import { Decoration, DecorationSet } from '@tiptap/pm/view'
 
 export interface FocusOptions {
   className: string
-  // 全部 | 最浅 | 最深
+  // 全部 | 最深 | 最浅
   mode: 'all' | 'deepest' | 'shallowest'
 }
 
@@ -40,7 +40,11 @@ export const FocusClasses = Extension.create<FocusOptions>({
             // Maximum Levels
             let maxLevels = 0
 
+            // 最深模式
             if (this.options.mode === 'deepest') {
+              // https://www.xheldon.com/tech/prosemirror-guide-chinese.html#indexing
+              // 对每一个后代节点调用给定的回调函数 f。当回调处理一个节点的时候返回 false ，则后续不会继续对该节点的子节点再调用该回调了。
+              // 注: 上述递归都是深度优先。
               doc.descendants((node, pos) => {
                 if (node.isText) {
                   return
